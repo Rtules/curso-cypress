42636fb71e96fd9ae0e7a10faab8103948c937b4
Rtules <rtulesqa@gmail.com>
2024-11-12 15:23:10 +0100

    corregir generacion de reporte

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require("cypress");
 const { configureAllureAdapterPlugins } =require('@mmisty/cypress-allure-adapter/plugins');
+
 module.exports = defineConfig({
+  retries: 1,
+  video: false,
   e2e: {
     setupNodeEvents(on, config) {
       configureAllureAdapterPlugins(on, config);
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,13 @@
+const { LoginMethots } = require("./pages/login/login.methods");
+
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    const usuario = 'rtules';
+    const contrasena ='rtules'
+    cy.visit('https://www.demoblaze.com/index.html')
+    cy.get('a[data-target="#logInModal"]').click()
+    LoginMethots.login(usuario, contrasena)
+    cy.get('a#nameofuser').should('contain.text', usuario)
+    cy.wait(5000)
   })
 })
