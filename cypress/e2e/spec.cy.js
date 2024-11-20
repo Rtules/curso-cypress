d38398a9780d3b4b563fb8f29cae23953eec0622
Rtules <rtulesqa@gmail.com>
2024-11-20 10:41:30 +0100

    Creacion de cart

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,12 +1,15 @@
-const { HomeMethods } = require("./pages/home/home.methods");
-const { LoginMethots } = require("./pages/login/login.methods");
+import { CartMethods } from './pages/cart/cart.methods';
+import { LoginMethots } from './pages/login/login.methods';
 
 describe('template spec', () => {
   it('passes', () => {
-  //  const usuario = 'rtules';
-  //const contrasena ='rtules'
+  const usuario = 'rtules';
+  const contrasena ='rtules'
     cy.visit('https://www.demoblaze.com/index.html')
-    HomeMethods.clicOnProductLink('Iphone 6 32gb')
-   cy.wait(10000)
+    cy.wait(30000)
+    CartMethods.clickOnDelete('Nokia lumia 1520')
+    cy.wait(30000)
+   
+   
   })
 })
