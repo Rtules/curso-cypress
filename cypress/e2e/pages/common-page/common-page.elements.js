d06c13786a202512298aa8baaae7927ad75ed335
Rtules <rtulesqa@gmail.com>
2024-11-20 13:22:04 +0100

    crear de pagina de common page

---
+++ cypress/e2e/pages/common-page/common-page.elements.js
@@ -0,0 +1,24 @@
+export class CommonPageElements {
+  static get topMenu(){
+    return {
+        get home(){
+            return cy.contains("a", "Home");
+        },
+        get contact(){
+            return cy.contains("a", "Contact");
+        },
+        get aboutUs(){
+            return cy.contains("a", "About us");
+        },
+        get cart(){
+            return cy.contains("a", "cart");
+        },
+        get login(){
+            return cy.contains("a", "log in");
+        },
+        get singnup(){
+            return cy.contains("a", "Sing up");
+        },
+    };
+  }  
+}
